47e357474ed047e94daefc30f1d9db45deb17b4d
dimitris <tartoufo1973@tartoufo1973-laptop.(none)>
2009-11-19 18:39:28 +0200

    Boring!!!!!

--- src/org/opentox/Resources/Algorithms/AlgorithmRdfFormater.java
+++ src/org/opentox/Resources/Algorithms/AlgorithmRdfFormater.java
@@ -1,9 +1,7 @@
 package org.opentox.Resources.Algorithms;
 
-
+import java.util.ArrayList;
 import org.opentox.Resources.AbstractResource;
-import org.restlet.data.MediaType;
-import org.restlet.representation.StringRepresentation;
 
 /**
  * Build an RDF representation for an Algorithm or a Model.
@@ -11,11 +9,9 @@ import org.restlet.representation.StringRepresentation;
  * @author Sopasakis Pantelis
  * @author Sarimveis Harry
  */
-public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
+public class AlgorithmRdfFormater {
 
-    private static final long serialVersionUID = 52795861750765264L;
-
-    private String
+    private String title,
             description,
             subject,
             type,
@@ -27,18 +23,42 @@ public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
             rights,
             date,
             format,
+            identifier,
             language,
             audience,
             provenance,
-            about
-            ;
-    
+            about,
+            rdfNamespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            dcNamespace = "http://purl.org/dc/elements/1.1/",
+            opentoxNamespace = "http://opentox.org/api.1-1/algorithm",
+            algorithmType;
+    private ArrayList<String> statisticsSupported;
+    private String[][] Parameters;
 
     public AlgorithmRdfFormater(String about) {
         this.about = about;
     }
 
-    
+    /**
+     * <b>Label:</b> Title<br/><br/>
+     * <b>Element Description:</b> The name given to the resource.
+     * Typically, a Title will be a name by which the resource is formally known.<br/><br/>
+     * <b>Guidelines for creation of content:</b>
+     * If in doubt about what constitutes the title, repeat the Title element and
+     * include the variants in second and subsequent Title iterations.
+     * If the item is in HTML, view the source document and make sure that the
+     * title identified in the title header (if any) is also included as a Title.<br/><br/>
+     * <b>Examples:</b>
+     * <pre> Title="A Pilot's Guide to Aircraft Insurance"
+     * Title="The Sound of Music"
+     * Title="Green on Greens"
+     * Title="AOPA's Tips on Buying Used Aircraft"</pre>
+     * @see http://dublincore.org/documents/usageguide/elements.shtml
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     /**
      * <b>Label:</b> Description<br/><br/>
@@ -193,7 +213,30 @@ public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
         this.format = format;
     }
 
-    
+    /**
+     * <b>Label:</b> Resource Identifier<br/><br/>
+     * <b>Element Description:</b> An unambiguous reference to the resource
+     * within a given context. Recommended best practice is to identify the
+     * resource by means of a string or number conforming to a formal
+     * identification system. Examples of formal identification systems
+     * include the Uniform Resource Identifier (URI) (including the
+     * Uniform Resource Locator (URL), the Digital Object
+     * Identifier (DOI) and the International Standard Book Number (ISBN).<br/><br/>
+     * <b>Guidelines for content creation:</b>
+     * This element can also be used for local identifiers (e.g. ID numbers
+     * or call numbers) assigned by the Creator of the resource to apply to
+     * a particular item. It should not be used for identification of the
+     * metadata record itself.<br/><br/>
+     * <b>Examples</b>
+     * <pre> Identifier="http://purl.oclc.org/metadata/dublin_core/"
+     * Identifier="ISBN:0385424728"
+     * Identifier="H-A-X 5690B" [publisher number]</pre>
+     * @param identifier The resource identifier
+     * @see http://dublincore.org/documents/usageguide/elements.shtml
+     */
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 
     /**
      * <b>Label:</b> Language<br/><br/>
@@ -275,7 +318,31 @@ public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
         this.dcNamespace = dcns;
     }
 
-    
+    /**
+     * Set the algorithm ontological type according to some formal ontology,
+     * e.g. algorithm:learning:regression:mlr
+     * @param algorithmType
+     */
+    public void setAlgorithmType(String algorithmType) {
+        this.algorithmType = algorithmType;
+    }
+
+    /**
+     * Define the characteristics of the algorithm. The <em>ArrayList&lt;String&gt;
+     * </em>element statisticsSupported is a list of the supported statistics by the
+     * algorithm. The String array <em>Parameters</em> is the set of tuning parameters
+     * of the algorithm, i.e. data that can be posted to the algorithm.<br/><br/>
+     * <b>Note:</b> Parameters is a N-by-3 matrix of type String. The first
+     * column contains the name of the parameter, the second the datatype and
+     * the third the default value.
+     * @param statisticsSupported
+     * @param Parameters
+     */
+    public void setAlgorithm(ArrayList<String> statisticsSupported,
+            String[][] Parameters) {
+        this.statisticsSupported = statisticsSupported;
+        this.Parameters = Parameters;
+    }
 
     private String opentoxAlgoritmElement() {
         StringBuilder builder = new StringBuilder();
@@ -307,9 +374,7 @@ public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
         return builder.toString();
     }
 
-
-    @Override
-    public StringRepresentation getStringRepresentation() {
+    public String rdfXmlRepresentation() {
         StringBuilder builder = new StringBuilder();
         builder.append(AbstractResource.xmlIntro);
 
@@ -338,9 +403,6 @@ public class AlgorithmRdfFormater extends AbstractAlgorithmFormater{
         builder.append(opentoxAlgoritmElement());
         builder.append("</rdf:Description>\n");
         builder.append("</rdf:RDF>\n\n");
-        return new StringRepresentation(builder.toString(),MediaType.APPLICATION_RDF_XML);
+        return builder.toString();
     }
-
-
-
 }
