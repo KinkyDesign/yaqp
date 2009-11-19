bc88067c980edd79b91e0fde2a408238f33921bf
dimitris <tartoufo1973@tartoufo1973-laptop.(none)>
2009-11-19 16:26:37 +0200

    Converter class updated.

--- src/org/opentox/util/converters/Converter.java
+++ src/org/opentox/util/converters/Converter.java
@@ -5,8 +5,15 @@
 
 package org.opentox.util.converters;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import weka.core.Instances;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 
 /**
  *
@@ -19,16 +26,56 @@ public class Converter extends AbstractConverter{
 
     private static final long serialVersionUID = 8525170373664066039L;
 
+    public void Converter(){
+    }
     
     @Override
     public void convert(final Instances instances, File dsdFile) {
+        int i, j, targetIdx;
         
-        throw new UnsupportedOperationException("Not supported yet.");
+        StringBuilder DsdString = new StringBuilder();
+        
+        
+        targetIdx = instances.classIndex();
+        System.out.println(targetIdx);
+        for(i=0; i<instances.numInstances(); i++){
+            DsdString.append(instances.instance(i).value(targetIdx) + " ");
+
+            for(j=0; j<targetIdx; j++){
+                DsdString.append((j+1) + ":" + instances.instance(i).value(j)  + " ");
+            }
+            for(j=targetIdx+1; j<instances.numAttributes(); j++){
+                DsdString.append(j + ":" + instances.instance(i).value(j));
+                if(j!=instances.numAttributes()){
+                    DsdString.append(" ");
+                }
+            }
+            DsdString.append("\n");
+        }
+
+        try {
+
+            FileWriter fstream = new FileWriter(dsdFile);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(DsdString.toString());
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 
     @Override
     public void convert(final File dsdFile, Instances instances) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static void main(String [] a) throws IOException{
+        Instances data = new Instances(new BufferedReader(new FileReader("/home/tartoufo1973/Documents/RESTfulWebServices/uploads/data/arff/dataSet-8")));
+        data.setClassIndex(1);
+        Converter asd = new Converter();
+        File output = new File("/home/tartoufo1973/Desktop/dsdoutput.txt");
+        asd.convert(data, output);
     }
 
 }
