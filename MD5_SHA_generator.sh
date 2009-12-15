85d2c3507774cb6644a7f19092da83ef3eec1210
Sopasakis Pantelis <chvng@mail.ntua.gr>
2009-12-15 19:39:03 +0200

    Updated PHP code and added a shell script that generates MD5 and SHA512 sums for all files in the project

---
+++ MD5_SHA_generator.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+FILES="*.java";
+SHA=sha512sum;
+MD5=md5sum;
+MD5FILE=md5sums;
+SHAFILE=sha512sums;
+
+if [ -e $MD5FILE ]
+then
+echo "Removing old file: $MD5FILE";
+rm $MD5FILE;
+fi
+
+if [ -e $SHAFILE ]
+then
+echo "Removing the old file: $SHAFILE"
+rm $SHAFILE;
+fi
+
+for file in  `find . -type f`
+ do
+   if [ ! -d $file ]
+   then
+    $MD5 $file >> $MD5FILE;
+    $SHA $file >> $SHAFILE
+   fi
+ done
+
+
